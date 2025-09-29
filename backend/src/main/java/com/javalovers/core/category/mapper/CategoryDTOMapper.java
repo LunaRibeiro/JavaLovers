050b09b89521a7b5e57669a4e29f299bb652398f
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2025-09-29 17:35:01 -0300

    feat: add mappers for Category domain (create, update, and DTO conversion)

---
+++ backend/src/main/java/com/javalovers/core/category/mapper/CategoryDTOMapper.java
@@ -0,0 +1,16 @@
+package com.javalovers.core.category.mapper;
+
+import com.javalovers.core.category.domain.dto.response.CategoryDTO;
+import com.javalovers.core.category.domain.entity.Category;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryDTOMapper {
+
+    public CategoryDTO convert(Category category) {
+        if(category == null) return null;
+        return new CategoryDTO(
+                category.getName()
+        );
+    }
+}
