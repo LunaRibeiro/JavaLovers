c2584040a67e06d385a6d7f896abd498b9dec6be
LunaRibeiro <lunasantos@alunos.utfpr.edu.br>
2025-10-09 16:15:15 -0300

    feat: Add CardRepository interface with JPA and specification support

---
+++ backend/src/main/java/com/javalovers/core/card/repository/CardRepository.java
@@ -0,0 +1,10 @@
+package com.javalovers.core.card.repository;
+
+import com.javalovers.core.card.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long>, JpaSpecificationExecutor<Card> {
+}
