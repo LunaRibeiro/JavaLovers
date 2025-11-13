07f9061e20db390c5098bd621ea24bf98d4a2095
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2025-11-13 14:30:56 -0300

    feat: add WithdrawalFormDTO and WithdrawalDTO to support withdrawal data transfer

---
+++ backend/src/main/java/com/javalovers/core/withdrawal/domain/dto/request/WithdrawalFormDTO.java
@@ -0,0 +1,10 @@
+package com.javalovers.core.withdrawal.domain.dto.request;
+
+import java.util.Date;
+
+public record WithdrawalFormDTO(
+        Date withdrawalDate,
+        Long beneficiaryId,
+        Long attendantUser
+) {
+}
