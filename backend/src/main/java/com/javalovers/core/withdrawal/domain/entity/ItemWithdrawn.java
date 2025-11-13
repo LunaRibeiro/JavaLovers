af60059691ae313a38b812b5af356284c2f4b1ed
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2025-11-13 15:36:45 -0300

    feat: add ItemWithdrawn entity to represent withdrawn items linked to Withdrawal and Item entities

---
+++ backend/src/main/java/com/javalovers/core/withdrawal/domain/entity/ItemWithdrawn.java
@@ -0,0 +1,34 @@
+package com.javalovers.core.withdrawal.domain.entity;
+
+import com.javalovers.core.item.domain.entity.Item;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "item_withdrawn")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemWithdrawn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_withdrawn_id")
+    private Integer itemWithdrawnId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "withdrawal_id", nullable = false)
+    private Withdrawal withdrawal;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+}
