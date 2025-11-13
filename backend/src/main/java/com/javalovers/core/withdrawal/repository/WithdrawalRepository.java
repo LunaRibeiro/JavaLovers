072962bcb1b326b587b4fcb79b9edf29aaf542b8
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2025-11-13 14:58:20 -0300

    feat: extend WithdrawalRepository with JpaSpecificationExecutor for advanced query support

--- backend/src/main/java/com/javalovers/core/withdrawal/repository/WithdrawalRepository.java
+++ backend/src/main/java/com/javalovers/core/withdrawal/repository/WithdrawalRepository.java
@@ -2,8 +2,9 @@ package com.javalovers.core.withdrawal.repository;
 
 import com.javalovers.core.withdrawal.domain.entity.Withdrawal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long>{
+public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long>, JpaSpecificationExecutor<Withdrawal> {
 }
